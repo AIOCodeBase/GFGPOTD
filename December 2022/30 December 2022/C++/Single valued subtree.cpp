612358dbd3805de2c621c279ed420977c713731c
Soumyadeep Mandal <imsamprodeveloper@gmail.com>
2022-12-30 11:03:48 +0530

    Single valued subtree

---
+++ December 2022/30 December 2022/C++/Single valued subtree.cpp
@@ -0,0 +1,145 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node
+{
+	int data;
+	Node* left;
+	Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+	Node* temp = new Node;
+	temp->data = val;
+	temp->left = NULL;
+	temp->right = NULL;
+
+	return temp;
+}
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+	// Corner Case
+	if (str.length() == 0 || str[0] == 'N')
+		return NULL;
+
+	// Creating vector of strings from input
+	// string after spliting by space
+	vector<string> ip;
+
+	istringstream iss(str);
+	for (string str; iss >> str; )
+		ip.push_back(str);
+
+	// Create the root of the tree
+	Node* root = newNode(stoi(ip[0]));
+
+	// Push the root to the queue
+	queue<Node*> queue;
+	queue.push(root);
+
+	// Starting from the second element
+	int i = 1;
+	while (!queue.empty() && i < ip.size()) {
+
+		// Get and remove the front of the queue
+		Node* currNode = queue.front();
+		queue.pop();
+
+		// Get the current node's value from the string
+		string currVal = ip[i];
+
+		// If the left child is not null
+		if (currVal != "N") {
+
+			// Create the left child for the current node
+			currNode->left = newNode(stoi(currVal));
+
+			// Push it to the queue
+			queue.push(currNode->left);
+		}
+
+		// For the right child
+		i++;
+		if (i >= ip.size())
+			break;
+		currVal = ip[i];
+
+		// If the right child is not null
+		if (currVal != "N") {
+
+			// Create the right child for the current node
+			currNode->right = newNode(stoi(currVal));
+
+			// Push it to the queue
+			queue.push(currNode->right);
+		}
+		i++;
+	}
+
+	return root;
+}
+
+
+// } Driver Code Ends
+
+class Solution {
+ public:
+  bool countSingleRec(Node* root, int& count) {
+    if (root == NULL) return true;
+
+    bool left = countSingleRec(root->left, count);
+    bool right = countSingleRec(root->right, count);
+
+    if (left == false || right == false) return false;
+
+    if (root->left && root->data != root->left->data) return false;
+
+    if (root->right && root->data != root->right->data) return false;
+
+    count++;
+    return true;
+  }
+
+ public:
+  int singlevalued(Node* root) {
+    int count = 0;
+
+    countSingleRec(root, count);
+
+    return count;
+  }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+
+	int t;
+	cin >> t;
+	getchar();
+	while (t--)
+	{
+
+		string inp;
+		getline(cin, inp);
+
+        Solution ob;
+        
+		struct Node* root = buildTree(inp);
+		cout << ob.singlevalued(root) << "\n";
+
+	}
+	return 0;
+}
+
+// } Driver Code Ends
---
+++ December 2022/30 December 2022/README.md
@@ -0,0 +1,43 @@
+# **Single valued subtree**
+
+## **Medium**
+
+Given a binary tree, count the number of Single Valued Subtrees. A Single Valued Subtree is one in which all the nodes have same value.
+
+**Example 1**
+
+```
+Input :
+              5
+             / \
+            1   5
+           / \   \
+          5   5   5
+Output : 4
+Explanation :
+There are 4 subtrees with single values.
+```
+
+**Example 2:**
+
+```
+Input:
+              5
+             / \
+            4   5
+           / \   \
+          4   4   5
+Output: 5
+Explanation:
+There are five subtrees with single values.
+```
+
+**Your task :**You don't have to read input or print anything. Your task is to complete the function singlevalued() which takes the root of the tree as input and returns the count of single valued subtrees.
+
+**Expected Time Complexity :** O(n)  
+**Expected Auxiliary Space :** O(n)
+
+**Constraints :**  
+1 &lt;= n &lt;= 10^5
+
+> ### **Problem URL: [Single valued subtree](https://practice.geeksforgeeks.org/problems/single-valued-subtree/1)**
