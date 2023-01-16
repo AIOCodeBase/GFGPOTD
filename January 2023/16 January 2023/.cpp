53ac2566119522630317fbd3217287f04b942d4b
unknown <madhavsetia24@gmail.com>
2023-01-16 14:52:13 +0530

    CPP file 16 JAN added

---
+++ January 2023/16 January 2023/.cpp
@@ -0,0 +1,54 @@
+Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User Function Template for C++ solution
+
+class Solution {
+  public:
+    vector<long long> nextLargerElement(vector<long long> &arr, int n){
+      vector<long long>ans;
+      stack<long long>st;
+      for(int i=n-1;i>=0;i--){
+      while(!st.empty() && st.top()<=arr[i])st.pop();
+      if(st.empty()){
+          ans.push_back(-1);
+          st.push(arr[i]);
+      }
+      else{
+          ans.push_back(st.top());
+          st.push(arr[i]);
+      }
+      }
+      reverse(ans.begin(),ans.end());
+      return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+
+        int n;
+        cin>>n;
+        vector<long long> arr(n);
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+
+        Solution ob;
+
+        vector <long long> res = ob.nextLargerElement(arr, n);
+        for (long long i : res) cout << i << " ";
+        cout<<endl;
+    }
+	return 0;
+}
+
+// } Driver Code Ends
