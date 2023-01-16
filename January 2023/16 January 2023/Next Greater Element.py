a4888c9cf4006c4f3e432553c442eee0b845f75c
khadija <kamrankhadijadj@gmail.com>
2023-01-16 20:34:12 +0500

    Added Next Greater Element code in python

---
+++ January 2023/16 January 2023/Next Greater Element.py
@@ -0,0 +1,47 @@
+#User function Template for python3
+
+
+class Solution:
+    def nextLargerElement(self,arr,n):
+        #code here
+        s = []
+        for i in range(len(arr)):
+            while s and s[-1].get("value") < arr[i]:
+                d = s.pop()
+                arr[d.get("ind")] = arr[i]
+            s.append({"value": arr[i], "ind": i})
+        while s:
+            d = s.pop()
+            arr[d.get("ind")] = -1
+        return arr
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases) :
+        n = int(input())
+
+        a = list(map(int,input().strip().split()))
+        obj = Solution()
+        res = obj.nextLargerElement(a,n)
+        for i in range (len (res)):
+            print (res[i], end = " ")
+        print ()
+# } Driver Code Ends
