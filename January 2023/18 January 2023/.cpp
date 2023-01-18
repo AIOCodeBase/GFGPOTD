7c8c365de01b071e14e7201bc95a3c21222cb20b
unknown <madhavsetia24@gmail.com>
2023-01-18 14:50:52 +0530

    CPP code 18 JAN added

---
+++ January 2023/18 January 2023/.cpp
@@ -0,0 +1,98 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void loopHere(Node* head, Node* tail, int position)
+{
+    if(position==0) return;
+    
+    Node* walk = head;
+    for(int i=1; i<position; i++)
+        walk = walk->next;
+    tail->next = walk;
+}
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+/*struct Node
+{
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+
+*/
+class Solution
+{
+    public:
+     //Function to find first node if the linked list has a loop.
+    int findFirstNode(Node* head)
+    {
+         Node*fast=head;
+         Node*slow=head;
+         while(fast && fast->next && slow){
+            fast=fast->next->next;
+            slow=slow->next;
+            if(fast==slow)break;
+         }
+         if(fast!=slow)return -1;
+         slow=head;
+         while(fast!=slow){
+             fast=fast->next;
+             slow=slow->next;
+         }
+         return slow->data;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, num;
+        cin>>n;
+
+        Node *head, *tail;
+        cin>> num;
+        head = tail = new Node(num);
+
+        for(int i=0 ; i<n-1 ; i++)
+        {
+            cin>> num;
+            tail->next = new Node(num);
+            tail = tail->next;
+        }
+
+        int pos;
+        cin>> pos;
+        loopHere(head,tail,pos);
+
+        Solution ob;
+        int ans = ob.findFirstNode(head);
+        cout<<ans<<"\n";
+    }
+	return 0;
+}
+// } Driver Code Ends
