360e7a27ef992ab284428e9c02110622ac722280
zaim <zaim1.hassani-adili@epitech.eu>
2023-01-20 18:44:05 +0100

    add solution to Maximum Weight Node in java

---
+++ January 2023/20 January 2023/Maximum Weight Node.java
@@ -0,0 +1,158 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class GFG{
+    static class FastReader{ 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader(){ 
+            br = new BufferedReader(new InputStreamReader(System.in)); 
+        } 
+  
+        String next(){ 
+            while (st == null || !st.hasMoreElements()){ 
+                try{ st = new StringTokenizer(br.readLine()); } catch (IOException  e){ e.printStackTrace(); } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        String nextLine(){ 
+            String str = ""; 
+            try{ str = br.readLine(); } catch (IOException e) { e.printStackTrace(); } 
+            return str; 
+        } 
+
+        Integer nextInt(){
+            return Integer.parseInt(next());
+        }
+
+        Long nextLong(){
+            return Long.parseLong(next());
+        }
+    }
+
+    public static void main(String[] args) throws IOException
+    {
+        FastReader sc = new FastReader();
+        PrintWriter out = new PrintWriter(System.out);
+        int t = sc.nextInt();
+        while(t-- > 0){
+            int N = sc.nextInt();
+            int Edge[] = new int[N];
+            for(int i = 0; i < N; i++)
+                Edge[i] = sc.nextInt();
+            Solution ob = new Solution();
+            int ans = ob.maxWeightCell(N, Edge);
+            out.println(ans);            
+        }
+        out.flush();
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    public int maxWeightCell(int N, int Edge[]){
+       Integer[] weights = Collections.nCopies(N, 0).toArray(new Integer[N]);
+       
+        if( N == 1) return 0;
+        for(int i= 0; i < N; i++){
+            if(Edge[i] != -1){
+                int index =Edge[i];
+                weights[index] += i;
+            }
+        }
+        int max = Collections.max(Arrays.asList(weights));
+        List<Integer> arrayList = Arrays.asList(weights);
+     
+      return arrayList.lastIndexOf(max);
+    }
+}//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class GFG{
+    static class FastReader{ 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader(){ 
+            br = new BufferedReader(new InputStreamReader(System.in)); 
+        } 
+  
+        String next(){ 
+            while (st == null || !st.hasMoreElements()){ 
+                try{ st = new StringTokenizer(br.readLine()); } catch (IOException  e){ e.printStackTrace(); } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        String nextLine(){ 
+            String str = ""; 
+            try{ str = br.readLine(); } catch (IOException e) { e.printStackTrace(); } 
+            return str; 
+        } 
+
+        Integer nextInt(){
+            return Integer.parseInt(next());
+        }
+
+        Long nextLong(){
+            return Long.parseLong(next());
+        }
+    }
+
+    public static void main(String[] args) throws IOException
+    {
+        FastReader sc = new FastReader();
+        PrintWriter out = new PrintWriter(System.out);
+        int t = sc.nextInt();
+        while(t-- > 0){
+            int N = sc.nextInt();
+            int Edge[] = new int[N];
+            for(int i = 0; i < N; i++)
+                Edge[i] = sc.nextInt();
+            Solution ob = new Solution();
+            int ans = ob.maxWeightCell(N, Edge);
+            out.println(ans);            
+        }
+        out.flush();
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    public int maxWeightCell(int N, int Edge[]){
+       Integer[] weights = Collections.nCopies(N, 0).toArray(new Integer[N]);
+       
+        for(int i= 0; i < N; i++){
+            if(Edge[i] != -1){
+                int index =Edge[i];
+                weights[index] += i;
+            }
+        }
+        int max = Collections.max(Arrays.asList(weights));
+        List<Integer> arrayList = Arrays.asList(weights);
+     
+      return arrayList.lastIndexOf(max);
+    }
+}
