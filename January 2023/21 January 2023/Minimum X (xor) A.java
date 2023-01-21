4bc23f5f74bd96277c36ea721830aea5258fa0a3
Rishav Raj <97666287+Rishav1707@users.noreply.github.com>
2023-01-21 12:39:43 +0530

    21Jan Java code added

---
+++ January 2023/21 January 2023/Minimum X (xor) A.java
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+
+            int a = Integer.parseInt(br.readLine().trim());
+            int b = Integer.parseInt(br.readLine().trim());
+
+            Solution ob = new Solution();
+            out.println(ob.minVal(a, b));
+        }
+        out.flush();
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public static int minVal(int a, int b) {
+        // code here
+        int a1 = countSetBit(a);
+        int b1 = countSetBit(b);
+        int ans = a;
+        if(a1==b1) {
+            
+        } else if(a1<b1) {
+            int idx=0;
+            while(a1!=b1) {
+                if(a%2==0) {
+                    a1++;
+                    ans+=(1<<idx);
+                }
+                idx++;
+                a/=2;
+            }
+        } else {
+            int idx=0;
+            while(a1!=b1) {
+                if(a%2==1) {
+                    a1--;
+                    ans-=(1<<idx);
+                }
+                idx++;
+                a/=2;
+            }
+        }
+        return ans;
+    }
+    
+    public static int countSetBit(int n) {
+        int ct=0;
+        while(n>0) {
+            if(n%2==1) ct++;
+            n/=2;
+        }
+        return ct;
+    }
+}
