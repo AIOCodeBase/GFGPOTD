d4e2cd4ea6aa91f1fd6fa3459481e34305ffe498
unknown <madhavsetia24@gmail.com>
2023-01-29 00:59:00 +0530

    JAVA CODE ADDED 29JAN

---
+++ January 2023/29 January 2023/.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int a;
+            a = Integer.parseInt(br.readLine());
+            
+            
+            int b;
+            b = Integer.parseInt(br.readLine());
+            
+            Solution obj = new Solution();
+            int res = obj.solve(a, b);
+            
+            out.println(res);
+            
+        }
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+ if(a==b){
+
+        return 0;
+   }
+        int k=a&b;
+
+        if(k!=a && k!=b)
+
+        return 2;
+
+        return 1;
+    }
+}
+
+
