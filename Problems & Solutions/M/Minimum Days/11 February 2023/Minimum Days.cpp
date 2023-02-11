50ebfaf1aa718bf596c38713ae2fb92b6c04ed69
Rishav Raj <97666287+Rishav1707@users.noreply.github.com>
2023-02-11 03:40:32 +0000

    11 Feb C++ code and README added

---
+++ Problems & Solutions/M/Minimum Days/11 February 2023/Minimum Days.cpp
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int getMinimumDays(int N,string S, vector<int> &P) {
+        
+        int minDays = 0;
+        
+        for (int i=0; i<N-1; i++){
+            
+            while (S[i] == S[i+1] && S[i] != '?'){
+                
+                S[P[minDays++]] = '?';
+            }
+        }
+        
+        return minDays;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int N;
+        scanf("%d",&N);
+        string S;cin>>S;
+        
+        vector<int> P(N);
+        Array::input(P,N);
+        
+        Solution obj;
+        int res = obj.getMinimumDays(N,S, P);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
