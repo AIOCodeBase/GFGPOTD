266aab22346f5ea2171db74a5394517721bae64a
Rishav Raj <97666287+Rishav1707@users.noreply.github.com>
2023-02-13 17:25:43 +0530

    13Feb C++ code added

---
+++ Problems & Solutions/A/Arithmetic Number/13 February 2023/Arithmetic Number.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int inSequence(int A, int B, int C){
+        
+        if(C==0) return A==B;
+        
+        int comm_diff = B-A;
+        
+        if(comm_diff%C==0 && comm_diff/C>=0)return 1;
+        
+        return 0;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int A, B, C;
+        cin>>A>>B>>C;
+        
+        Solution ob;
+        cout<<ob.inSequence(A, B, C)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
