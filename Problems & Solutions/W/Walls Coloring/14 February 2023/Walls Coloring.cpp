81b6106a5ef9078a40a01effde2491461b7c1529
Soumyadeep Mandal <49950107+ImSaMPro@users.noreply.github.com>
2023-02-15 13:20:52 +0530

    Update Walls Coloring.cpp

--- Problems & Solutions/W/Walls Coloring/14 February 2023/Walls Coloring.cpp
+++ Problems & Solutions/W/Walls Coloring/14 February 2023/Walls Coloring.cpp
@@ -11,7 +11,6 @@ using namespace std;
 class Solution{   
 public:
     int minCost(vector<vector<int>> &colors, int N) {
-        // Write your code here.
         for (int i=1; i<N; i++){
             colors[i][0] += min(colors[i-1][1], colors[i-1][2]);
             colors[i][1] += min(colors[i-1][0], colors[i-1][2]);
