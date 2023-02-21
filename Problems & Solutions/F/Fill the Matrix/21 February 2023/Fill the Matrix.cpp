21d4e3c449819ff1113bab02b9fc7f463d172a34
Rishav Raj <97666287+Rishav1707@users.noreply.github.com>
2023-02-21 22:21:47 +0530

    21 Feb C++ code added

---
+++ Problems & Solutions/F/Fill the Matrix/21 February 2023/Fill the Matrix.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{   
+public:
+    int minIteration(int N, int M, int x, int y){  
+        
+        int hmax = max(x-1, N-x);
+        int wmax = max(y-1, M-y);
+        return (hmax + wmax);
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N, M, x, y;
+        cin >> N >> M;
+        cin >> x >> y;
+        
+        Solution ob;
+        cout << ob.minIteration(N, M, x, y) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
