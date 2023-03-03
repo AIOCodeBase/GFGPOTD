85741eb917609970c30f115b117dbd671363cfb1
Rishav Raj <97666287+Rishav1707@users.noreply.github.com>
2023-03-03 23:13:34 +0530

    03 Feb C++ code added

---
+++ Problems & Solutions/C/Cutting Rectangles/03 February 2023/Cutting Rectangles.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    vector<long long int> minimumSquares(long long int L, long long int B)
+    {
+        long long int K = __gcd(L,B);
+        long long int N = L/K * B/K;
+        return {N,K};
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long int L, B;
+        cin>>L>>B;
+        
+        Solution ob;
+        vector<long long int> ans = ob.minimumSquares(L, B);
+        cout<<ans[0]<<" "<<ans[1]<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
